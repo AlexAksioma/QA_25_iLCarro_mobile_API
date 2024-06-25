52b6e4c76251a9ce5ff9e44d0b98aa30f34390ac
AlexAksioma <alexmedqwerty@gmail.com>
2024-06-25 21:49:38 +0300

    created structure

--- src/main/java/screens/BaseScreen.java
+++ src/main/java/screens/BaseScreen.java
@@ -1,2 +1,36 @@
-package screens;public class BaseScreen {
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseScreen {
+    AppiumDriver<MobileElement> driver;
+    public BaseScreen(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time*1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean textInElementPresent(MobileElement element, String text, int time){
+        try{
+            return new WebDriverWait(driver, time)
+                    .until(ExpectedConditions.textToBePresentInElement(element, text));
+        }catch (TimeoutException e){
+            System.out.println("created exception");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/main/java/screens/SearchScreen.java
+++ src/main/java/screens/SearchScreen.java
@@ -1,2 +1,10 @@
-package screens;public class SearchScreen {
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class SearchScreen extends BaseScreen{
+    public SearchScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
 }
